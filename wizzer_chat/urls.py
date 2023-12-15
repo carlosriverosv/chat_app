8b85054d6b6f3dccd608b3bb6e2f612e3d14f54d
Carlos Riveros <carlosriverosv@gmail.com>
2023-12-15 10:03:23 -0500

    django rest library and initial config included

--- wizzer_chat/urls.py
+++ wizzer_chat/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+api_path = path("api/", include('chat.api.urls'))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    api_path,
 ]
